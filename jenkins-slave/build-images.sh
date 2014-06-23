175709b119cb286f8082d1c1f7b29edbe5b400c0
Mathieu Guillaume <mg@nuxeo.com>
2014-06-23 18:02:18 +0200

    Add deb packaging tools to jenkins container

--- jenkins-slave/build-images.sh
+++ jenkins-slave/build-images.sh
@@ -11,6 +11,8 @@ cp /opt/build/hudson/.netrc slave-config/tmpfiles/jenkins_netrc
 cp /opt/build/hudson/.s3cfg slave-config/tmpfiles/jenkins_s3cfg
 scp hudson@blancheneige:.ssh/id_rsa slave-config/tmpfiles/jenkins_id_rsa
 scp hudson@blancheneige:bin/slave.jar slave-config/tmpfiles/slave.jar
+cp /opt/build/keystores/gpg/nxpkg-secret.key slave-config/tmpfiles/nxpkg-secret.key
+cp /opt/build/keystores/gpg/nxpkg-public.key slave-config/tmpfiles/nxpkg-public.key
 
 docker build -t nuxeo/jenkins-slave-config slave-config
 

a65b902cab6c17301e233401803df1e7e760111d
Mathieu Guillaume <mg@nuxeo.com>
2014-07-03 17:40:58 +0200

    Jenkins slave: add authorized keys and oracle JDBC drivers

--- jenkins-slave/build-images.sh
+++ jenkins-slave/build-images.sh
@@ -1,6 +1,13 @@
 #!/bin/bash -e
 
+
+if [ ! -d slave-base/tmpfiles ]; then
+    mkdir slave-base/tmpfiles
+    mkdir slave-base/tmpfiles/m2repo
+fi
+cp -a /opt/build/m2_repo_seed/* slave-base/tmpfiles/m2repo/
 docker build -t nuxeo/jenkins-slave-base slave-base
+rm -rf slave-base/tmpfiles
 
 if [ ! -d slave-config/tmpfiles ]; then
     mkdir slave-config/tmpfiles
@@ -11,6 +18,8 @@ cp /opt/build/hudson/.netrc slave-config/tmpfiles/jenkins_netrc
 cp /opt/build/hudson/.s3cfg slave-config/tmpfiles/jenkins_s3cfg
 scp hudson@blancheneige:.ssh/id_rsa slave-config/tmpfiles/jenkins_id_rsa
 scp hudson@blancheneige:bin/slave.jar slave-config/tmpfiles/slave.jar
+cat slave-config/files/jenkins_id_rsa.pub /opt/build/hudson/authorized_keys/* > slave-config/tmpfiles/authorized_keys
+
 cp /opt/build/keystores/gpg/nxpkg-secret.key slave-config/tmpfiles/nxpkg-secret.key
 cp /opt/build/keystores/gpg/nxpkg-public.key slave-config/tmpfiles/nxpkg-public.key
 

a1a16b7fc31e3241447e8e2af4fa8c7a0b79753f
Mathieu Guillaume <mg@nuxeo.com>
2014-10-28 11:29:24 +0100

    NXBT-818: provision slaves with instance.clid

--- jenkins-slave/build-images.sh
+++ jenkins-slave/build-images.sh
@@ -16,6 +16,7 @@ chmod 0700 slave-config/tmpfiles
 cp /opt/build/hudson/m2remote/settings.xml slave-config/tmpfiles/
 cp /opt/build/hudson/.netrc slave-config/tmpfiles/jenkins_netrc
 cp /opt/build/hudson/.s3cfg slave-config/tmpfiles/jenkins_s3cfg
+cp /opt/build/hudson/instance.clid slave-config/tmpfiles/
 scp hudson@blancheneige:.ssh/id_rsa slave-config/tmpfiles/jenkins_id_rsa
 scp hudson@blancheneige:bin/slave.jar slave-config/tmpfiles/slave.jar
 cat slave-config/files/jenkins_id_rsa.pub /opt/build/hudson/authorized_keys/* > slave-config/tmpfiles/authorized_keys
